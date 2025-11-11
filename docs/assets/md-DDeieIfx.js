969d5674aef124b474e96d5023a90ca243c87d13
Владислав Хоменко <r87-vlad@monowork.pro>
2025-11-11 12:41:28 +0300

    Build Slidev presentation for GitHub Pages

---
+++ docs/assets/md-DDeieIfx.js
@@ -0,0 +1,10 @@
+import{b as o,o as n,w as l,g as t,i,v as c,x as m,T as e}from"./modules/vue-BOt6Q5Me.js";import{_ as p,a as f,b as d}from"./image.vue_vue_type_script_setup_true_lang-D_0DQgDB.js";import{u as h,f as u}from"./slidev/context-DfdWMxyi.js";import"./index-wg4bECKk.js";import"./modules/shiki-D84VB3Oy.js";const w={__name:"slides.md__slidev_32",setup(_){const{$clicksContext:a,$frontmatter:r}=h();return a.setup(),(b,s)=>(n(),o(d,c(m(e(u)(e(r),31))),{default:l(()=>[s[0]||(s[0]=t("div",{class:"flex justify-between items-start mb-6"},[t("img",{src:p,class:"h-15",alt:"Левое изображение"}),t("img",{src:f,class:"h-10",alt:"Правое изображение"})],-1)),s[1]||(s[1]=t("div",{class:"flex flex-col justify-center items-center h-100"},[t("h1",null,"Спасибо за внимание!"),t("h2",null,"Вопросы и обсуждение"),t("div",{class:"pt-8 text-sm opacity-50"},[t("div",null,"Полезные ссылки:"),t("div",{class:"flex justify-center gap-4 mt-2"},[t("a",{href:"https://github.com/typestack/class-transformer",target:"_blank"},"class-transformer"),t("a",{href:"https://www.typescriptlang.org/docs/handbook/decorators.html",target:"_blank"},"TypeScript Decorators")])])],-1)),i(` <div class="pt-12">
+  <div class="flex justify-center gap-4">
+    <a href="https://github.com/typestack/class-transformer" target="_blank" class="text-xl slidev-icon-btn !border-none !hover:text-white">
+      <carbon:logo-github />
+    </a>
+    <a href="https://sli.dev" target="_blank" class="text-xl slidev-icon-btn !border-none !hover:text-white">
+      <carbon:logo-slack />
+    </a>
+  </div>
+</div> `)]),_:1},16))}};export{w as default};
